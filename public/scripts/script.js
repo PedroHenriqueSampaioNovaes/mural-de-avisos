2e6c8f563a1f25a4e9bfe39afc10e41feab86c21
Pedro Henrique Sampaio de Novaes <92189897+PedroHenriqueSampaioNovaes@users.noreply.github.com>
2023-01-30 22:27:59 -0300

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,7 @@
 // document.addEventListener('DOMContentLoaded', () => updatePosts());
 
 async function updatePosts() {
-  const response = await fetch('http://localhost:3000/api/all');
+  const response = await fetch('https://mural-de-avisos-gules.vercel.app/api/all');
   const posts = JSON.parse(await response.json());
   console.log(posts)
 
@@ -32,7 +32,7 @@ function newPost() {
     },
     body: JSON.stringify({ title, description }),
   }
-  fetch('http://localhost:3000/api/new', options).then(() => {
+  fetch('https://mural-de-avisos-gules.vercel.app/api/new', options).then(() => {
     updatePosts();
     document.querySelector('#title').value = '';
     document.querySelector('#desc').value = '';
