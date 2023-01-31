f67c875a128b750d273bb03ab798fbf2c62e668d
Pedro Henrique Sampaio de Novaes <92189897+PedroHenriqueSampaioNovaes@users.noreply.github.com>
2023-01-30 22:21:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ const app = express();
 app.use('/api', apiRoute);
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-// app.listen(PORT, () => {
-//   console.log('Servidor rodando na porta', PORT);
-// });
+app.listen(PORT, () => {
+  console.log('Servidor rodando na porta', PORT);
+});
